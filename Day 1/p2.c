d49c2efa63d8f680499be61b1dfaa6bae223e5f9
ianiket005 <ianiket.patel007@gmail.com>
2026-06-01 20:29:01 +0530

    h

--- Day 1/p2.c
+++ Day 1/p2.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-int main(){
+int main(){  
     int num,i;
     printf("Enter a number: ");
     scanf("%d",&num);
     for(i=1;i<=10;i++){
         printf("%d x %d = %d\n",num,i,num*i);
     }
-    return 0;13
-}
+    return 0;
+}ṇṇṇ
