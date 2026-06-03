498969d0034c6d63e6ed535b93006982d8e539e7
ianiket005 <ianiket.patel007@gmail.com>
2026-06-03 18:10:57 +0530

    d3

--- DAY 3/P1.c
+++ DAY 3/P1.c
@@ -5,15 +5,15 @@ int main() {
     int num;
     bool isPrime = true;
 
-    // Input from user
+    
     printf("Enter a positive integer: ");
     scanf("%d", &num);
-34
-    // 0 and 1 are not prime numbers
+
+    
     if (num <= 1) {
         isPrime = false;
     } else {
-        // Check for factors from 2 up to num / 2 (or sqrt(num))
+        
         for (int i = 2; i * i <= num; i++) {
             if (num % i == 0) {
                 isPrime = false; 
