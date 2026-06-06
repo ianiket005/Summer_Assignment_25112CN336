57de02ff583e748d44be29c7b91aadf7f8e0e3da
ianiket005 <ianiket.patel007@gmail.com>
2026-06-06 18:27:58 +0530

    D6

---
+++ DAY 6/P3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    unsigned int num;
+    int count = 0;
+
+    printf("Enter a number: ");
+    scanf("%u", &num);
+
+    while (num > 0) {
+        count += num & 1;   
+        num >>= 1;          
+    }
+
+    printf("Number of set bits: %d\n", count);
+
+    return 0;
+}
