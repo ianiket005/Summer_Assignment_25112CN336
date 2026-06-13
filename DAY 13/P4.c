d5fcaac8f00f1431d4eeae2ae49a5a93e97be8ff
ianiket005 <ianiket.patel007@gmail.com>
2026-06-13 17:21:30 +0530

    D13

---
+++ DAY 13/P4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+    int arr[100], n, i;
+    int evenCount = 0, oddCount = 0;
+
+    
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+
+        
+        if (arr[i] % 2 == 0)
+            evenCount++;
+        else
+            oddCount++;
+    }
+
+    
+    printf("Total Even elements = %d\n", evenCount);
+    printf("Total Odd elements = %d\n", oddCount);
+
+    return 0;
+}
