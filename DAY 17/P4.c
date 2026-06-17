9306d9e02cf87da3d6e69354e77881d50b6202c8
ianiket005 <ianiket.patel007@gmail.com>
2026-06-17 16:14:16 +0530

    DAY 17

---
+++ DAY 17/P4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    int n1, n2, i, j;
+    printf("Enter size of first array: ");
+    scanf("%d", &n1);
+    int arr1[n1];
+    printf("Enter %d elements of first array:\n", n1);
+    for(i = 0; i < n1; i++) {
+        scanf("%d", &arr1[i]);
+    }
+
+    printf("Enter size of second array: ");
+    scanf("%d", &n2);
+    int arr2[n2];
+    printf("Enter %d elements of second array:\n", n2);
+    for(i = 0; i < n2; i++) {
+        scanf("%d", &arr2[i]);
+    }
+
+    printf("Common elements are:\n");
+    for(i = 0; i < n1; i++) {
+        for(j = 0; j < n2; j++) {
+            if(arr1[i] == arr2[j]) {
+                
+                int k, found = 0;
+                for(k = 0; k < i; k++) {
+                    if(arr1[k] == arr1[i]) {
+                        found = 1;
+                        break;
+                    }
+                }
+                if(!found) {
+                    printf("%d ", arr1[i]);
+                }
+                break;
+            }
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
